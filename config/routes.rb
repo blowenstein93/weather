abf0e22632315d0748d4c44292bb335e36cb203c
Brad Lowenstein <blowenstein93@gmail.com>
2014-07-24 16:26:02 -0700

    background

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :show]
+  resources :users, only: [:new, :create, :show]
   root 'welcome#index'
   resources :weathers, only: [:new, :show]
   get "signin" => "users#signin"
