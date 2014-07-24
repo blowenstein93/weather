600761abafe75d834031e859c6746a6523523c92
Brad Lowenstein <blowenstein93@gmail.com>
2014-07-24 15:14:25 -0700

    404

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :show]
   root 'welcome#index'
-  resources :weathers
+  resources :weathers, only: [:new, :show]
   get "signin" => "users#signin"
   get "search" => "users#search"
 
