f26810badd94f26345697cad46689a5f31ed2394
Brad Lowenstein <blowenstein93@gmail.com>
2014-07-26 00:06:57 -0700

    javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 
 
 function hide_signin(){
-    var ele = document.getElementsByTagName("li")[2];
+    var ele = document.getElementsByTagName("li")[3];
         if (ele.className.indexOf("hide") == -1) {
             ele.className =  "hide";
         } else if (ele.className.indexOf("hide") > -1) {
@@ -32,3 +32,44 @@ $(document).ready(function(){
         hide_signin();
     }
 });
+
+var fade = function(node) {
+    var level = 1;
+    var opacity = 1.0;
+    var step = function(){
+        var hex = level.toString(16);
+        opacity -= 0.05;
+        node.style.backgroundColor = "rgba(99, 99, 122, " + opacity + ")";
+
+        if (level < 20) {
+            level += 1;
+            setTimeout(step, 50);
+        }
+    };
+    setTimeout(step, 100);
+}
+
+
+var hover = function(node_set){
+    var i;
+    var length = node_set.length;
+    for (i =0; i < length; i++){
+        node_set[i].onmouseover = function(i){
+            return function() {
+                console.log("on" + node_set[i])
+                node_set[i].style.backgroundColor = "#6E6E6E";
+            };
+        }(i);
+        node_set[i].onmouseout = function(i){
+            return function() {
+                console.log("off" + node_set[i])
+                node_set[i].style.backgroundColor = "#FFF";
+            };
+        }(i);
+    }
+}
+
+window.onload = function() {
+    fade(document.getElementsByClassName("nav_bar")[0]);
+    hover(document.getElementsByClassName("nav_el"));
+}
