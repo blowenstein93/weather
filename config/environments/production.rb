7794e2852f7f80e9da624fa0384bf92ab562f3fd
Brad Lowenstein <blowenstein93@gmail.com>
2014-07-28 18:11:45 -0700

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js )
+  config.assets.precompile += "application.js"
+  config.assets.precompile += "application.css"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
