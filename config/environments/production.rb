236232e275fcbc0319a461ecc4cf966ebdcc10c8
Brad Lowenstein <blowenstein93@gmail.com>
2014-07-28 18:09:53 -0700

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile = ['*.js', 'application.css', '*.css.erb']
+  config.assets.precompile = ['application.js', 'application.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
