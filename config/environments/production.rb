e365b2830a16d2c04c57166046f02e08344dcbd3
Brad Lowenstein <blowenstein93@gmail.com>
2014-08-07 16:52:36 -0700

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header =  ‘X-Accel-Redirect’
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
